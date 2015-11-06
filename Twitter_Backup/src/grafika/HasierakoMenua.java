332aed874f6411cc3a7fa54ce5f80f5e682581b7
antton <antton@antton>
2015-11-06 11:13:13 +0100

    add user botoia jarri

--- Twitter_Backup/src/grafika/HasierakoMenua.java
+++ Twitter_Backup/src/grafika/HasierakoMenua.java
@@ -67,62 +67,56 @@ public class HasierakoMenua extends JFrame implements ActionListener{
 	/**
 	 * Launch the application.
 	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//
-//					frame.setVisible(true);
-//					frame.setMinimumSize(new Dimension(450, 300));
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
+	//	public static void main(String[] args) {
+	//		EventQueue.invokeLater(new Runnable() {
+	//			public void run() {
+	//				try {
+	//
+	//					frame.setVisible(true);
+	//					frame.setMinimumSize(new Dimension(450, 300));
+	//				} catch (Exception e) {
+	//					e.printStackTrace();
+	//				}
+	//			}
+	//		});
+	//	}
 
 	/**
 	 * Create the frame.
 	 */
 	public HasierakoMenua() {
-		
-		//int altuera=300;
-		
-		
+
+
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, zabalera, altuera);
-		
-		//setJMenuBar(menuBar);
-		//contentPane.add(menuBar, BorderLayout.NORTH);
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout());
 		contentPane.add(scrollPane,BorderLayout.CENTER);
-		//menuUser.add(new JMenuItem("userproba"));
-		
-		
+
+
 		contentPane.add(gehiago, BorderLayout.SOUTH);
 		gehiago.setVisible(false);
 		gehiago.addActionListener(this);
 		gehiago.setActionCommand("20+");
-		
-		
+
+
 		scrollPane.setViewportView(textArea);
 		textArea.setEditable(false);
 		textArea.setColumns(20);
 		textArea.setWrapStyleWord(true);
-		
+
 		JPanel contentPane1 = new JPanel();
 		contentPane.add(contentPane1, BorderLayout.NORTH);
 		contentPane1.setLayout(new BorderLayout(0, 0));
 		contentPane1.add(menuBar);
-		
+
 		JMenuItem mntmTweet = new JMenuItem("tweet");
-		//contentPane.add(mntmTweet, BorderLayout.NORTH);
 		mntmTweet.addActionListener(this);
 		mntmTweet.setActionCommand("tweet");
-		//scrollPane.add
 		JMenuItem mntmFav = new JMenuItem("fav");
 		JMenuItem mntmRetweet = new JMenuItem("Retweet");
 		JMenuItem mntmDM = new JMenuItem("Direct message");
@@ -131,13 +125,10 @@ public class HasierakoMenua extends JFrame implements ActionListener{
 		menuBar.add(mntmFav);
 		menuBar.add(mntmRetweet);
 		menuBar.add(mntmDM);
-		
+
 		JMenu menuUser =new JMenu("users");
 		menuUser.setMinimumSize(new Dimension(50, 20));
-		//contentPane.add(menuUser, BorderLayout.NORTH);
-		//menuUser.add(new JMenuItem("user1"));
 		menuBar.add(menuUser);
-		//scrollPane.getVerticalScrollBar().setValue(0);
 		LinkedList<String> lk=User.getUser().getTwitterUsers();
 		Iterator<String> it=lk.iterator();
 		JMenuItem item=null;
@@ -152,66 +143,46 @@ public class HasierakoMenua extends JFrame implements ActionListener{
 		menuUser.add(item);
 		item.addActionListener(this);
 		item.setActionCommand("adduser");
-		
-		
+
+
 	} 
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
 		if (arg0.getActionCommand().equals("adduser")){
 			System.out.println("add user");
 		}
 		if (arg0.getActionCommand().equals("tweet")){
-//		TwitterConect tc =new TwitterConect();
-//		tc.login();
-//		tc.getTwitts();
-		try {
-			Long zenb=new Long(TwitterController.getTwitterController().getAzkenId());
-			Stack<String> st=TwitterController.getTwitterController().tweetakIkusi(zenb,zenb+2000000000);
-			String mesage=new String();
-			if(arg0.getActionCommand().equals("20+")){
-				mesage=textArea.getText();
-				//System.out.println(mesage);
-				mesage =mesage+"\n"+"\n-*"+st.pop();
-				
-				
-			}else{
-				mesage ="-"+st.pop();
+			//		TwitterConect tc =new TwitterConect();
+			//		tc.login();
+			//		tc.getTwitts();
+			try {
+				Long zenb=new Long(TwitterController.getTwitterController().getAzkenId());
+				Stack<String> st=TwitterController.getTwitterController().tweetakIkusi(zenb,zenb+2000000000);
+				String mesage=new String();
+				if(arg0.getActionCommand().equals("20+")){
+					mesage=textArea.getText();
+					mesage =mesage+"\n"+"\n-*"+st.pop();
+				}else{
+					mesage ="-"+st.pop();
+				}
+				int color=0;
+				while(!st.isEmpty()){
+					String ms=st.pop();
+					if(!ms.equals(null)){
+						mesage=mesage+ms+" "+"\n"+"\n-*";}
+				}
+				textArea.setText(mesage);
+				gehiago.setVisible(true);
+				scrollPane.getVerticalScrollBar().setValue(0);
+				Point p = new Point(1,1);
+				scrollPane.getViewport().setViewPosition(p); 
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			int color=0;
-			while(!st.isEmpty()){
-				String ms=st.pop();
-				//System.out.println(ms);
-				if(!ms.equals(null)){
- 				 mesage=mesage+ms+" "+"\n"+"\n-*";}
-//				if(color%2==0){
-//					textArea.setForeground(Color.BLUE);
-//					textArea.setText(mesage);
-//				}else{
-//					textArea.setForeground(Color.RED);
-//					textArea.setText(mesage);
-//				}
-//				color++;
-			}
-			textArea.setText(mesage);
-			
-			gehiago.setVisible(true);
-//			scrollPane.setCorner(scrollPane.UPPER_LEFT_CORNER, null );
-			scrollPane.getVerticalScrollBar().setValue(0);
-			///scrollPane.setC
-			//scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getMaximum());
-			Point p = new Point(1,1);
-			scrollPane.getViewport().setViewPosition(p); 
-			
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		}	
 	}
 
-	
+
 }
