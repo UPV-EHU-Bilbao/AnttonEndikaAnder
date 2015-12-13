abf650e9bbfb6c3f3df9a5376549140f1b8205f9
ander <ander@ander-X553MA>
2015-12-13 21:47:20 +0100

    javadoc komentarioak

--- Twitter_Backup/src/controller/TwitterController.java
+++ Twitter_Backup/src/controller/TwitterController.java
@@ -48,20 +48,7 @@ public class TwitterController {
 		}
 		return (Long)null;
 	}
-
-//	public Long getAzkenTweetId(String taula){
-//		//datubasean sartutako azken id-a itzultzen du gehiago egotekotan deskarga bertatik jarraitzeko
-//		try {			
-//			Object[] params = new Object[0];
-//			ResultSet request = DB.getDb().select("SELECT id FROM MyTweets ORDER BY id DESC LIMIT 1",params);	
-//			request.next();
-//			return new Long(request.getLong(1));
-//
-//		} catch (Exception e) {
-//			System.out.println("Error:  "+e);
-//		}
-//		return null;
-//	}
+	
 	
 	public ArrayList<String> tweetakIkusi(String tUser, boolean hasieratu) throws SQLException {
 		//pantailaratutako azken id-tik abiaratuta beste 20 hartzen ditu
@@ -88,22 +75,8 @@ public class TwitterController {
 		return st;
 	}
 
-//	public ArrayList<String> lehentweetakIkusi(String tUser) throws SQLException {
-//		//Honek lehen 20-ak hartzen ditu (datubasetik), hurrengoak hartzeko beste sql sententzia bat erabili behar delako
-//		//ResultSet request = Dd.getDd().select("SELECT id,mesage FROM MyTweets WHERE twitterUser='"+tUser+"' ORDER BY id DESC LIMIT 20");
-//		Object[] params = new Object[1];
-//		params[0]=tUser;
-//
-//		ResultSet request = DB.getDb().select("SELECT id,mesage FROM MyTweets WHERE twitterUser=? ORDER BY id DESC LIMIT 20", params);
-//		ArrayList<String> st=new ArrayList<String>();
-//
-//		while(request.next()){
-//			st.add(request.getString(2));
-//			azkenTweetId = request.getLong(1);
-//		}
-//		return st;
-//	}
-//
+
+
 //	public ArrayList<String> tweetakIkusi(String tUser) throws SQLException {
 //		//pantailaratutako azken id-tik abiaratuta beste 20 hartzen ditu
 //		Object[] params = new Object[2];
@@ -146,24 +119,6 @@ public class TwitterController {
 		return st;
 	}
 
-//	public ArrayList<String[]> lehenFavkIkusi(String tUser) throws SQLException {
-//		//Honek lehen 20-ak hartzen ditu (datubasetik), hurrengoak hartzeko beste sql sententzia bat erabili behar delako
-//		//ResultSet request = Dd.getDd().select("SELECT id,mesage FROM MyTweets WHERE twitterUser='"+tUser+"' ORDER BY id DESC LIMIT 20");
-//		Object[] params = new Object[1];
-//		params[0]=tUser;
-//
-//		ResultSet request = DB.getDb().select("SELECT id,mesage,name FROM Fav WHERE twitterUser=? ORDER BY id DESC LIMIT 20", params);
-//		ArrayList<String[]> st=new ArrayList<String[]>();
-//		
-//		while(request.next()){
-//			String[] stlis=new String[2];
-//			stlis[0]=request.getString(2);
-//			stlis[1]="@"+request.getString(3);
-//			st.add(stlis);
-//			azkenFavId = request.getLong(1);
-//		}
-//		return st;
-//	}
 
 	public void tweetaGorde(String userName, Status status){
 		Object[] params = new Object[4];
@@ -329,6 +284,7 @@ public class TwitterController {
 	}
 	
 	public void tarteaSartu(String tUser, String taula, Long etena, Long helmuga){
+		//Deskarga gelditu deneko eta heldu behar zeneko id-ak datubasean gordetzen dira
 		Object[] params = new Object[4];
 		params[0]=taula;
 		params[1]=etena;
@@ -338,6 +294,7 @@ public class TwitterController {
 	}
 	
 	public void tarteaEzabatu(String tUser, String taula, Long etena){
+		//Deskargatuko den tartea ezabatzen du.
 		Object[] params = new Object[3];
 		params[0]=tUser;
 		params[1]=taula;
